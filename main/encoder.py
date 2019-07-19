e7674d43b922768861b3f40a8732e6295d1b782e
Amirreza <salamat.amirreza@gmail.com>
2019-07-19 12:24:46 -0400

    minor fixes for encoder

--- main/encoder.py
+++ main/encoder.py
@@ -7,13 +7,13 @@ import pickle
 @debug
 @timer
 def counter(mongo_client, db):
-    db.user_follower.update_many({'expired': {'$exists': False}}, {'$set': {'expired': '0'}})
+    db.user_follower.update_many({'read': {'$exists': False}}, {'$set': {'read': '0'}})
     try: 
         with open('./main/counts', 'rb') as f:
             counts = pickle.load(f)
     except FileNotFoundError:
         return reset_counter(mongo_client, db)
-    docs = db.user_follower.find({'expired': '0'}, {'user_id': 1, 'follower_user_id': 1})
+    docs = db.user_follower.find({'read': '0'}, {'user_id': 1, 'follower_user_id': 1})
     for doc in docs: 
         if doc['user_id'] in counts:
             counts[doc['user_id']] += 1
@@ -23,18 +23,19 @@ def counter(mongo_client, db):
             counts[doc['follower_user_id']] += 1
         else:
             counts[doc['follower_user_id']] = 1
-        db.user_follower.update_one({'_id': doc['_id']}, {'$set': {'expired': '1'}})
+        db.user_follower.update_one({'_id': doc['_id']}, {'$set': {'read': '1', 'expired': '0'}})
     with open('./main/counts', 'wb') as file:
         pickle.dump(counts, file)
     return integer_encoding(mongo_client, db, counts)
+    
 def reset_counter(mongo_client, db): 
-    db.user_follower.update_many({}, {'$set': {'expired': '0'}})
-    docs = db.user_follower.find({'expired': '0'}, {'user_id': 1, 'follower_user_id': 1})
+    db.user_follower.update_many({}, {'$set': {'read': '0'}})
+    docs = db.user_follower.find({'read': '0'}, {'user_id': 1, 'follower_user_id': 1})
     counts = cnt.Counter()
     for doc in docs: 
         counts[doc['user_id']] += 1
         counts[doc['follower_user_id']] += 1
-        db.user_follower.update_one({'_id': doc['_id']}, {'$set': {'expired': '1'}})
+        db.user_follower.update_one({'_id': doc['_id']}, {'$set': {'read': '1', 'expired': '0'}})
     with open('./main/counts', 'wb') as file:
         pickle.dump(counts, file)
     return integer_encoding(mongo_client, db, counts)
@@ -48,8 +49,6 @@ def integer_encoding(mongo_client, db, counts):
     reversed_dictionary = {v: k for k, v in dictionary.items()}
     with open('./main/reversed_dictionary', 'wb') as file:
         pickle.dump(reversed_dictionary, file)
+
 if __name__ == "__main__":
-    # reset_counter(mongo_client, db)
-    with open('./main/counts', 'rb') as f:
-        counts = pickle.load(f)
-    integer_encoding(mongo_client, db, counts)
+    reset_counter(mongo_client, db)
