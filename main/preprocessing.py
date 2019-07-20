24439d39a703c37f0f44c84f03efdd1bdb2df661
Amirreza <salamat.amirreza@gmail.com>
2019-07-20 13:25:12 -0400

    minor changes

--- main/preprocessing.py
+++ main/preprocessing.py
@@ -1,18 +1,17 @@
-import collections as cnt
-from env import mongo_client, db
+import collections 
+from env import db
 from helper_functions import timer, debug
-import pandas 
 import pickle
 
 @debug
 @timer
-def counter(mongo_client, db):
+def counter(db):
     db.user_follower.update_many({'read': {'$exists': False}}, {'$set': {'read': '0'}})
     try: 
         with open('./main/counts', 'rb') as f:
             counts = pickle.load(f)
     except FileNotFoundError:
-        return reset_counter(mongo_client, db)
+        return reset_counter(db)
     docs = db.user_follower.find({'read': '0'}, {'user_id': 1, 'follower_user_id': 1})
     for doc in docs: 
         counts[doc['user_id']] += 1
@@ -20,25 +19,25 @@ def counter(mongo_client, db):
         db.user_follower.update_one({'_id': doc['_id']}, {'$set': {'read': '1', 'expired': '0'}})
     with open('./main/counts', 'wb') as file:
         pickle.dump(counts, file)
-    return integer_encoding(mongo_client, db, counts)
+    return integer_encoding(db, counts)
 
 @debug
 @timer
-def reset_counter(mongo_client, db): 
-    db.user_follower.update_many({}, {'$set': {'read': '0'}})
+def reset_counter(db): 
+    db.user_follower.update_many({}, {'$set': {'read': '0', 'expired': '0', 'visited': '0'}})
     docs = db.user_follower.find({'read': '0'}, {'user_id': 1, 'follower_user_id': 1})
-    counts = cnt.Counter()
+    counts = collections.Counter()
     for doc in docs: 
         counts[doc['user_id']] += 1
         counts[doc['follower_user_id']] += 1
         db.user_follower.update_one({'_id': doc['_id']}, {'$set': {'read': '1', 'expired': '0'}})
     with open('./main/counts', 'wb') as file:
         pickle.dump(counts, file)
-    return integer_encoding(mongo_client, db, counts)
+    return integer_encoding(db, counts)
 
 @debug
 @timer
-def integer_encoding(mongo_client, db, counts):
+def integer_encoding(db, counts):
     dictionary = {}
     for code, count in enumerate(counts.most_common()):
         dictionary[count[0]] = code
@@ -47,17 +46,24 @@ def integer_encoding(mongo_client, db, counts):
     reversed_dictionary = {v: k for k, v in dictionary.items()}
     with open('./main/reversed_dictionary', 'wb') as file:
         pickle.dump(reversed_dictionary, file)
+    return input_transform(db, dictionary)
 
 @debug
 @timer
-def graph_generator(mongo_client, db, dictionary):
-    # db.user_follower.update_many({}, {'$set': {'expired': '0'}})
+def input_transform(db, dictionary):
+    docs = db.user_follower.find({'read': '1', 'expired': '1', 'visited': '0'}, {'user_id': 1, 'follower_user_id': 1})
+    for doc in docs: 
+        db.user_follower.update_one({'_id': doc['_id']}, 
+                {'$set': {'coded_user_id': dictionary[doc['user_id']], 'coded_follower_user_id': dictionary[doc['follower_user_id']]}})
+    return graph_generator(db, dictionary)
+
+@debug
+@timer
+def graph_generator(db, dictionary):
     docs = db.user_follower.find({'read': '1', 'expired': '0'}, {'user_id': 1, 'follower_user_id': 1})
     for doc in docs: 
         db.follower_graph.update_one({'user_id': dictionary[doc['user_id']]}, {'$push': {'followers': dictionary[doc['follower_user_id']]}}, upsert=True)
-        db.user_follower.update_one({'_id': doc['_id']}, {'$set': {'expired': '1'}})
+        db.user_follower.update_one({'_id': doc['_id']}, {'$set': {'expired': '1'}})        
 
 if __name__ == "__main__":
-    with open('./main/dictionary', 'rb') as file:
-        dictionary = pickle.load(file)
-    graph_generator(mongo_client, db, dictionary)
+    reset_counter(db)
